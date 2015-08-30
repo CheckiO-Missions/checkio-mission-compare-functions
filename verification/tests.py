9694feab2e38ed8a4461f8e5709d1b1a60dbb3db
jtokaz <jtokaz@gmail.com>
2015-08-30 16:06:47 -0500

    Update tests.py

--- verification/tests.py
+++ verification/tests.py
@@ -1,17 +1,35 @@
-"""
-TESTS is a dict with all you tests.
-Keys for this will be categories' names.
-Each test is dict with
-    "input" -- input data for user function
-    "answer" -- your right answer
-    "explanation" -- not necessary key, it's using for additional info in animation.
+init_code = """
+if not "checkio" in USER_GLOBAL:
+    raise NotImplementedError("Where is 'checkio'?")
+
+checkio = USER_GLOBAL['checkio']
 """
 
+PASS_CODE = """
+RET['code_result'] = True, "Ok"
+"""
+
+
+def prepare_test(middle_code, test_code, show_code, show_answer):
+    if show_code is None:
+        show_code = middle_code
+    return {"test_code": {"python-3": init_code + middle_code + test_code,
+                          "python-27": init_code + middle_code + test_code},
+            "show": {"python-3": show_code,
+                     "python-27": show_code},
+            "answer": show_answer}
+
+
 TESTS = {
-    "Basics": [
-        {
-            "input": "$5.34",
-            "answer": lambda:0
-        }          
-    ]
-}
+    "1. Init": [
+        prepare_test(
+            'f = lambda: 0\n'
+            'g = lambda: 0\n'
+            'a = (1,2)\n'
+            'b = {}\n'
+            'c = checkio(f,g)\n'
+            'r = c(1,2)\n',
+            'RET["code_result"] = (r == (0,"same"))',
+            'SHOWC',
+            'SHOWA')
+    ]}
