2fd7c3d2ba0ec77f66a114eaa3266f7a00bc6678
jtokaz <jtokaz@gmail.com>
2015-08-30 16:29:10 -0500

    Update referee.py

--- verification/referee.py
+++ verification/referee.py
@@ -28,11 +28,12 @@ checkio.referee.cover_codes
 
 from checkio.signals import ON_CONNECT
 from checkio import api
-from checkio.referees.io import CheckiOReferee
+from checkio.referees.code import CheckiORefereeCode
 from checkio.referees import cover_codes
 from checkio.referees import checkers
 
 from tests import TESTS
+
 
 def checker(result, test_data):
     return result['code_result']
