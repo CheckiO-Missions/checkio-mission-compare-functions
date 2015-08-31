f4714f84e507c20c0d5a456816afd815a5270c6a
jtokaz <jtokaz@gmail.com>
2015-08-30 21:25:55 -0500

    Update tests.py

--- verification/tests.py
+++ verification/tests.py
@@ -40,6 +40,17 @@ TESTS = {
                      '  elif x<0:\n'
                      '    return -x\n'
                      'c = checkio(f,g)\n'
+                     'result = c(2.3)\n',
+                     "RET['code_result'] = (result==(2.3,'same')), str(result)",
+                     None,
+                     "checkio(f,g)(2.3) = (2.3,'same')"),                     
+        prepare_test('f = lambda x: abs(x)\n'
+                     'def g(x):\n'
+                     '  if x>0:\n'
+                     '    return x\n'
+                     '  elif x<0:\n'
+                     '    return -x\n'
+                     'c = checkio(f,g)\n'
                      'result = c(-1)\n',
                      "RET['code_result'] = (result==(1,'same')), str(result)",
                      None,
@@ -126,89 +137,16 @@ TESTS = {
                      'result = c()\n',
                      "RET['code_result'] = (result==('hello_world','g_error')), str(result)",
                      None,
-                     "checkio(f,g)() = ('hello world','g_error')"),                       
-    ],
-    "4. Names": [
-        prepare_test(
-            'f = Friends(({"nikola", "sophia"}, {"stephen", "robot"}, {"sophia", "pilot"}))\n'
-            'n = f.names()\n',
-            'RET["code_result"] = (n == {"nikola", "sophia", "robot", "pilot", "stephen"}, str(n))',
-            'f = Friends(({"nikola", "sophia"}, {"stephen", "robot"}, {"sophia", "pilot"}))\n'
-            'f.names()',
-            '{"nikola", "sophia", "robot", "pilot", "stephen"}'),
-        prepare_test(
-            'f = Friends(({"nikola", "sophia"}, {"stephen", "robot"}, {"sophia", "pilot"}))\n'
-            'f.remove({"stephen", "robot"})\n'
-            'n = f.names()\n',
-            'RET["code_result"] = (n == {"nikola", "sophia", "pilot"}, str(n))',
-            'f = Friends(({"nikola", "sophia"}, {"stephen", "robot"}, {"sophia", "pilot"}))\n'
-            'f.remove({"stephen", "robot"})\n'
-            'f.names()',
-            '{"nikola", "sophia", "pilot"}'),
-
-    ],
-    "5. Connected": [
-        prepare_test(
-            'f = Friends(({"nikola", "sophia"}, {"stephen", "robot"}, {"sophia", "pilot"}))\n'
-            'n = f.connected("nikola")\n',
-            'RET["code_result"] = (n == {"sophia"}, str(n))',
-            'f = Friends(({"nikola", "sophia"}, {"stephen", "robot"}, {"sophia", "pilot"}))\n'
-            'f.connected("nikola")',
-            '{"sophia"}'),
-        prepare_test(
-            'f = Friends(({"nikola", "sophia"}, {"stephen", "robot"}, {"sophia", "pilot"}))\n'
-            'n = f.connected("sophia")\n',
-            'RET["code_result"] = (n == {"nikola", "pilot"}, str(n))',
-            'f = Friends(({"nikola", "sophia"}, {"stephen", "robot"}, {"sophia", "pilot"}))\n'
-            'f.connected("sophia")',
-            '{"nikola", "pilot"}'),
-        prepare_test(
-            'f = Friends(({"nikola", "sophia"}, {"stephen", "robot"}, {"sophia", "pilot"}))\n'
-            'n = f.connected("DDD")\n',
-            'RET["code_result"] = (n == set(), str(n))',
-            'f = Friends(({"nikola", "sophia"}, {"stephen", "robot"}, {"sophia", "pilot"}))\n'
-            'f.connected("DDD")',
-            'set()'),
-        prepare_test(
-            'f = Friends(({"nikola", "sophia"}, {"stephen", "robot"}, {"sophia", "pilot"}))\n'
-            'f.add({"sophia", "stephen"})\n'
-            'f.remove({"sophia", "nikola"})\n'
-            'n = f.connected("sophia")\n',
-            'RET["code_result"] = (n == {"stephen", "pilot"}, str(n))',
-            'f = Friends(({"nikola", "sophia"}, {"stephen", "robot"}, {"sophia", "pilot"}))\n'
-            'f.add({"sophia", "stephen"})\n'
-            'f.remove({"sophia", "nikola"})\n'
-            'f.connected("sophia")\n',
-            '{"stephen", "pilot"}'),
-
-
+                     "checkio(f,g)() = ('hello world','g_error')"),
+        prepare_test('def f(hello="hello",world="WORLD"):\n'
+                     '  return hello + " " + world\n'
+                     'def g(hello="hello",world="world"):\n'
+                     '  return hello + " " + world\n'
+                     'c = checkio(f,g)\n'
+                     'result = c("ahoi")\n',
+                     "RET['code_result'] = (result==('ahoi WORLD','different')), str(result)",
+                     None,
+                     "checkio(f,g)() = ('ahoi WORLD','different')"),                      
     ]
-
-
-
-
-    #     prepare_test(test="str(Building(1, 1, 2, 2))",
-    #                  answer="Building(1, 1, 2, 2, 10)", ),
-    #     prepare_test(test="str(Building(0.2, 1, 2, 2.2, 3.5))",
-    #                  answer="Building(0.2, 1, 2, 2.2, 3.5)", ),
-    # ],
-    # "3. Corners": [
-    #     prepare_test(test="Building(1, 1, 2, 2).corners()",
-    #                  answer={"south-west": [1, 1], "north-west": [3, 1], "north-east": [3, 3],
-    #                          "south-east": [1, 3]}),
-    #     prepare_test(test="Building(100.5, 0.5, 24.3, 12.2, 3).corners()",
-    #                  answer={"north-east": [112.7, 24.8], "north-west": [112.7, 0.5],
-    #                          "south-west": [100.5, 0.5], "south-east": [100.5, 24.8]}),
-    # ],
-    # "4. Area, Volume": [
-    #     prepare_test(test="Building(1, 1, 2, 2, 100).area()",
-    #                  answer=4),
-    #     prepare_test(test="Building(100, 100, 135.5, 2000.1).area()",
-    #                  answer=271013.55),
-    #     prepare_test(test="Building(1, 1, 2, 2, 100).volume()",
-    #                  answer=400),
-    #     prepare_test(test="Building(100, 100, 135.5, 2000.1).volume()",
-    #                  answer=2710135.5),
-    # ]
 
 }
